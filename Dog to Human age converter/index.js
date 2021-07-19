8d89b34bfacde5e227f69eb696e9b938bc1fe2ee
csabhi2 <81946950+csabhi2@users.noreply.github.com>
2021-07-19 19:22:17 +0530

    Update index.js

--- Dog to Human age converter/index.js
+++ Dog to Human age converter/index.js
@@ -1,3 +1,3 @@
-var dogage = prompt("Hhow old is your dog?")
+var dogage = prompt("How old is your dog?")
 var humanage = (dogage - 2) * 4 + 21;
 alert("Your dog is "  + humanage + " years old in human age");
